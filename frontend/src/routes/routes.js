0a8b58af6e1706afba7fee66eec25563c8249461
GK <jaivigneshgk.g@esko.com>
2024-02-13 01:33:01 +0530

    Patched some merge effects

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,81 +1,49 @@
-import {
-  Drawer,
-  Hidden,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  makeStyles,
-  Paper,
-} from "@material-ui/core";
-import { Home } from "@material-ui/icons";
-import clsx from "clsx";
-import React from "react";
-import { Fragment } from "react";
-import AppsIcon from "@material-ui/icons/Apps";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router";
+import { Home } from "../components/Home";
+import { Login } from "../components/Login";
+import  UserManagementPage  from "../components/Admin/UserManagementPage.jsx";
+import Table from "../components/Table";
+import AuthLayout from "../layouts/Auth";
+import DashboardLayout from '../layouts/Dashboard';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "100%",
-    overflowY: "auto",
+export const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: () =>  <Redirect to="/home" />,
   },
-  list: {
-    width: 250,
+  {
+    path: "/auth",
+    component: AuthLayout,
+    routes: [
+      {
+        path: "/auth/login",
+        exact: true,
+        component: Login,
+      },
+    ],
   },
-  fullList: {
-    width: "auto",
-  },
-}));
-
-
-function NavBar(props) {
-  const { openMenu, closeMenu, className, ...rest } = props;
-  const classes = useStyles();
-
-  return (
-    <Fragment>
-      <Hidden lgUp>
-        <Drawer
-          anchor="left"
-          variant="temporary"
-          onClose={closeMenu}
-          open={openMenu}
-        >
-          <div {...rest} className={clsx(classes.root, className)}></div>
-        </Drawer>
-      </Hidden>
-      <Hidden mdDown>
-        <Paper
-          {...rest}
-          className={clsx(classes.root, className)}
-          elevation={1}
-          square
-        >
-          <List>
-            <ListItem component={Link} to="/">
-              <ListItemIcon>
-                <Home />
-              </ListItemIcon>
-              <ListItemText>Home</ListItemText>
-            </ListItem>
-            <ListItem component={Link} to="/table">
-              <ListItemIcon>
-                <AppsIcon />
-              </ListItemIcon>
-              <ListItemText>Table</ListItemText>
-            </ListItem>
-            <ListItem component={Link} to="/usermanagement">
-              <ListItemIcon>
-                <AppsIcon />
-              </ListItemIcon>
-              <ListItemText>Manage Users</ListItemText>
-            </ListItem>
-          </List>
-        </Paper>
-      </Hidden>
-    </Fragment>
-  );
-}
-
-export default NavBar;
+  {
+    route: '*',
+    component: DashboardLayout,
+    routes: [
+      {
+        path: "/home",
+        exact: true,
+        component: (props) => {return props.isAuthenticated ? <Home /> : <Redirect to="/auth/login" />}
+          ,
+      },
+      {
+        path: "/usermanagement",
+        exact: true,
+        component: (props) => {return props.isAuthenticated ? <UserManagementPage /> : <Redirect to="/auth/login" />}
+          ,
+      },
+      {
+        path:"/table",
+        exact: true,
+        component: (props) => {return props.isAuthenticated ? <Table /> : <Redirect to="/auth/login" />}
+      }
+    ]
+  }
+];
